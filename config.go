33e649f4b5d5b1a3924fdddd2f32c6b3c64c66f5
zimbatm <zimbatm@zimbatm.com>
2018-11-07 22:50:04 +0100

    move xdg into it's own mini package

--- config.go
+++ config.go
@@ -8,6 +8,7 @@ import (
 	"strings"
 
 	toml "github.com/BurntSushi/toml"
+	"github.com/direnv/direnv/xdg"
 )
 
 type Config struct {
@@ -41,7 +42,7 @@ func LoadConfig(env Env) (config *Config, err error) {
 
 	config.ConfDir = env[DIRENV_CONFIG]
 	if config.ConfDir == "" {
-		config.ConfDir = XdgConfigDir(env, "direnv")
+		config.ConfDir = xdg.ConfigDir(env, "direnv")
 	}
 	if config.ConfDir == "" {
 		err = fmt.Errorf("Couldn't find a configuration directory for direnv")
